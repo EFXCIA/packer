deb08e764a6fe6a510c0ec33e2e398e4f6823262
James Johnson <james.johnson@equifax.com>
2016-09-21 11:36:31 -0400

    Add remote repos sooner rather than later

--- efxcia-update.sh
+++ efxcia-update.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -ex
 
+git remote add mitchellh https://github.com/mitchellh/packer || :
+git remote add taliesins https://github.com/taliesins/packer.git || :
+
 git checkout master
 git fetch -a -t -p
 git pull --all
 
-git remote add mitchellh https://github.com/mitchellh/packer || :
 git merge mitchellh/master
-git push efxcia master
+git push origin master
 
-git remote add taliesins https://github.com/taliesins/packer.git || :
 git checkout HyperV
 git merge taliesins/HyperV
-git push efxcia HyperV
+git push origin HyperV
 
 git checkout master
